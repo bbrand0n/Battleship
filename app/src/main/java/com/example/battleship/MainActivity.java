ce3a1467726020b5b336202d3fc8650f53653e52
Brandon Gibbons <brandonginbons@icloud.com>
2021-11-18 16:33:40 -0500

    2nd - brandon

--- app/src/main/java/com/example/battleship/MainActivity.java
+++ app/src/main/java/com/example/battleship/MainActivity.java
@@ -1,20 +1,30 @@
 package com.example.battleship;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.os.Debug;
+import android.text.InputType;
 import android.util.Log;
+import android.util.LogPrinter;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import android.widget.VideoView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
@@ -23,24 +33,29 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     // Video background
     VideoView videoView;
 
     // Username input and button to submit
     EditText nameIn;
-    String uName;
-    Button find_room, create_game;
+    String uName, otherName;
+    Button find_room, create_game, settings;
+    DocumentReference player1, player2, newGame;
 
 
 
@@ -80,6 +95,42 @@ public class MainActivity extends AppCompatActivity {
         nameIn =       (EditText) findViewById(R.id.nameIn);
         find_room =    (Button) findViewById(R.id.find_room);
         create_game =  (Button) findViewById(R.id.create_game);
+        settings =     (Button) findViewById(R.id.settings);
+        Button quit =  (Button) findViewById(R.id.quit);
+
+
+
+
+
+        // -----------  CREATE GAME  ------------
+        // Create Game button
+        create_game.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Extract string
+                uName = nameIn.getText().toString();
+
+                // Create new player object
+                Player p = new Player(uName);
+
+
+                // Add player to database
+                player1 = addUser(p, db);
+
+
+                // Create new game
+                newGame = createGame(player1, db);
+
+
+                // Launch activity
+                Intent i = new Intent(MainActivity.this, NewGameActivity.class);
+                i.putExtra("p1name", p.getName());
+                i.putExtra("p2name", "Joining...");
+                startActivity(i);
+            }
+        });
+
+
 
 
 
@@ -96,50 +147,112 @@ public class MainActivity extends AppCompatActivity {
                 Player p = new Player(uName);
 
                 // Add player to database
-                addUser(p, db);
+                DocumentReference player2 = addUser(p, db);
+
+                // Display alert box to get other username --------
+                AlertDialog.Builder builder = new AlertDialog
+                        .Builder(MainActivity.this,
+                        R.style.Base_Theme_AppCompat_Dialog_Alert);
+                builder.setTitle("Join Game");
+                builder.setMessage("Enter opponents username");
+
+                // Make the view
+                final EditText input = new EditText(MainActivity.this);
+                input.setInputType(InputType.TYPE_CLASS_TEXT);
+                builder.setView(input);
+
+                // FIND button
+                builder.setPositiveButton("Find", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        otherName = input.getText().toString();
+                        dialog.cancel();
+                        joinGame(otherName, player2, db);
+                    }
+                });
+                // CANCEL button
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                // Show alert dialogue
+                builder.show();
 
             }});
 
 
-
-        // -----------  CREATE GAME  ------------
-        // Create Game button
-        create_game.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Extract string
-                uName = nameIn.getText().toString();
-
-                // Create new player object
-                Player p = new Player(uName);
-
-                // Add player to database
-                DocumentReference player1 = addUser(p, db);
-
-                // Create new game
-                DocumentReference newGame = createGame(player1, db);
-
-                // Launch new activity
-                Intent i = new Intent(MainActivity.this, NewGameActivity.class);
-                i.putExtra("player1_name", player1.getId() );
-                i.putExtra("gameId",       newGame.getId());
-                startActivity(i);
-            }
-        });
+//        db.collection("games")
+//                .whereEqualTo("isOpen", true)
+//                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+//                        for(QueryDocumentSnapshot doc : value){
+//
+//                            value.getDocuments().get(0).getReference().set(doc);
+//
+//                            // Launch activity
+//                            Intent i = new Intent(MainActivity.this, NewGameActivity.class);
+//                            i.putExtra("p1name", doc.get(String.valueOf(player1)).toString());
+//                            i.putExtra("p2name", doc.get(String.valueOf(player2)).toString());
+//                            startActivity(i);
+//                        }
+//
+//                    }
+//                });
 
 
 
-        // -------- QUIT ------------
-        Button quit = (Button) findViewById(R.id.quit);
-        quit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-                System.exit(0);
-            }
-        });
+
+
 
     }
+
+    // --------- CREATE GAME -----------
+    public DocumentReference createGame(DocumentReference player1, FirebaseFirestore db) {
+
+        // Create game map
+        Map<String, Object> game = new HashMap<>();
+        game.put("created", new Timestamp(new Date()));
+        game.put("isOpen", true);
+        game.put("player1", player1.getId());
+
+
+        // Add to database
+        db.collection("games")
+                .document(player1.getId())
+                    .set(game);
+
+
+        // Reference to game
+        DocumentReference newGame =  db.collection("games")
+                                    .document(player1.getId());
+
+
+
+        // Return game reference
+        return newGame;
+
+    }
+
+
+    // -------- SETTINGS ---------
+    public void settings(View v){
+        Intent i = new Intent(MainActivity.this, TestActivity.class);
+        startActivity(i);
+        // TODO: fix
+    }
+
+
+    // -------- QUIT ------------
+    public void quit(View v){
+        finish();
+        System.exit(0);
+    }
+
+
 
 
     public DocumentReference addUser(Player player, FirebaseFirestore db) {
@@ -161,46 +274,43 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public DocumentReference createGame(DocumentReference player1, FirebaseFirestore db) {
-
-        // Create game
-        Map<String, Object> game = new HashMap<>();
-        game.put("created", new Timestamp(new Date()));
-        game.put("isOpen", true);
-        game.put("player1", player1);
-
-        // Add to database, auto-generating game ID
-        return db.collection("games").add(game).getResult();
-
-    }
-
-    public void joinGame(CollectionReference games, DocumentReference player2, FirebaseFirestore db) {
-
-        // Create query to find open games
-        Query q = games.whereEqualTo("isOpen", true);
-
-        // Get open game
-        q.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-
-                // If game is found, update game attributes
-                if(task.isSuccessful()) {
-                    // Retrieve reference to the found game
-                    DocumentReference newGame = task.getResult()
-                            .getDocuments().get(0).getReference();
-
-                    // Update isOpen and players
-                    newGame.update("isOpen", false);
-                    newGame.update("player2", player2);
-                }
-                else{
-                    // TODO: display error: unable to find game
-                }
 
 
-            }
-        });
+    public void joinGame(String player1name, DocumentReference player2, FirebaseFirestore db) {
+
+        //
+        player1 = db.collection("games").document(player1name);
+
+        // Create query to find the game
+//        Query q = db.collection("games").whereEqualTo("player1", player1name);
+//        q.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//            @Override
+//            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                if(task.isSuccessful()) {
+//                    Log.d("GREAT", "Joined game successfully: "
+//                            + task.getResult().getDocuments().get(0));
+//                }
+//                else {
+//                    Log.d("BAD", "Unable to find player");
+//                    // TODO: go back to main menu
+//                }
+//            }
+//        });
+
+        // Get reference to game
+        //DocumentReference game = q.get().getResult().getDocuments().get(0).getReference();
+        DocumentReference game = db.collection("games").document(player1name);
+
+        // Update fields
+        game.update("isOpen", false);
+        game.update("player2", player2.getId());
+
+        Intent i = new Intent(MainActivity.this, NewGameActivity.class);
+        i.putExtra("p2name", player2.getId());
+        i.putExtra("p1name", player1.getId());
+        startActivity(i);
+
+
 
     }
 
--- app/src/main/java/com/example/battleship/NewGameActivity.java
+++ app/src/main/java/com/example/battleship/NewGameActivity.java
@@ -1,14 +1,33 @@
 package com.example.battleship;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.MetadataChanges;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.Transaction;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class NewGameActivity extends AppCompatActivity {
 
-    String player1, gameId;
+    String player1, player2, gameId;
+    DocumentReference game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +37,65 @@ public class NewGameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_game);
 
 
+
         // -------- RETRIEVE INFORMATION --------
         Intent i = getIntent();
-        player1  = i.getStringExtra("player1_name");
-        gameId   = i.getStringExtra("gameId");
+        player1  = i.getStringExtra("p1name");
+        player2  = i.getStringExtra("p2name");
+
 
 
         // -------- DISPLAY INFO -----------
         TextView p1 = (TextView) findViewById(R.id.p1);
-        p1.setText(player1);
+        TextView p2 = (TextView) findViewById(R.id.p2);
+
+
+
+
+        // -------- GET DATABASE ---------
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        game = db.collection("games").document(player1);
+
+
+        game.addSnapshotListener(MetadataChanges.INCLUDE, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                if(value.exists() && value != null){
+                    if(value.get("player1") != null){
+                        game.update("player1", value.get("player1").toString());
+                        p1.setText("Player 1:  " + player1);
+                    }
+                    if(value.get("player2") != null){
+                        game.update("player2", value.get("player2").toString());
+                        p2.setText("Player 2:  " + player2);
+                    }
+
+                }
+
+            }
+        });
+
+        // -------- UPDATE ACTIVITY WHEN NEW EVENT HAPPENS ----------
+//        db.collection("games")
+//                .whereEqualTo("player1", player1)
+//                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+//
+//
+//
+//                        for(QueryDocumentSnapshot doc : value){
+//                            //game.set(value.getDocuments().get(0).getData());
+//                            game.update(doc.getData());
+//                            p1.setText("Player 1:  " + player1);
+//                            p2.setText("Player 2:  " + player2);
+//                        }
+//                    }
+//                });
+
+
+
     }
+
+
 }
